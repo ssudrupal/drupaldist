f8e97e1c83a443dfb41fcc02ccbe77ab7c221690
Eric Eisenhart <eric.eisenhart@sonoma.edu>
2015-08-14 16:55:26 -0700

    Add a git commit with diff thing...

--- build.sh
+++ build.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
 DATE=$(date +%Y%m%d)
-LOCK="ssu-lock-$DATE.make"
+LOCK=ssu-lock.make
+DATELOCK="ssu-lock-$DATE.make"
 
 rm -f $LOCK
 
 git pull
 drush make --no-build ssu.make --lock=$LOCK
+git add $LOCK
 
-git commit -m auto $LOCK
 perl -p -i -e 'if (m/^projects\[panopoly\]\[version\]/ and not m/7.x/) { s/ = "/ = "7.x-/; }' $LOCK
-git commit -m 'version fix' $LOCK
+
+cp -f $LOCK $DATELOCK
+git add $DATELOCK
+git commit -m auto $DATELOCK
 
 CORE=$(grep '= "core"' $LOCK | cut -d[ -f2 | cut -d] -f1)
 COREVER=$(grep "projects\[$CORE\]\[version\]" $LOCK | cut -d= -f2 | cut -d'"' -f2)
@@ -25,6 +29,7 @@ chgrp -R drupal $OUTPUTDIR
 chmod -R g+w $OUTPUTDIR
 find $OUTPUTDIR -type d -print0 | xargs --null chmod g+s
 
+git commit -v $LOCK
 
 echo; echo ; echo
 echo === Your new distribution is: $OUTPUTDIR
