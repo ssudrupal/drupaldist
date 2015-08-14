f299fa643dfaad2a809c80d0cb6b630103193bdb
Eric Eisenhart <eric.eisenhart@sonoma.edu>
2015-08-14 16:20:52 -0700

    Automatically fix broken version thing from makefile maker. It's a bug, so should become irrelevant later.

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ rm -rf ssu-test
 
 git pull
 drush make --no-build ssu.make --lock=ssu-lock.make
+
 git commit -m auto ssu-lock.make
+perl -p -i.bak -e 'if (m/^projects\[panopoly\]\[version\]/ and not m/7.x/) { s/ = "/ = "7.x-/; }' ssu-lock.make
+git commit -m 'version fix' ssu-lock.make
 
 drush make ssu-lock.make ssu-test
