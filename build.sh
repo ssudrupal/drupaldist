2bc101b5f3f5da2c027fe760865529c33232bd87
Eric Eisenhart <eric.eisenhart@sonoma.edu>
2015-08-14 16:29:23 -0700

    Make the build do all the things

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-rm -rf ssu-test
+CORE=$(grep '= "core"' ssu-lock.make | cut -d[ -f2 | cut -d] -f1)
+COREVER=$(grep "projects\[$CORE\]\[version\]" ssu-lock.make | cut -d= -f2 | cut -d'"' -f2)
+DATE=$(date +%Y%m%d)
 
 git pull
 drush make --no-build ssu.make --lock=ssu-lock.make
@@ -9,4 +11,11 @@ git commit -m auto ssu-lock.make
 perl -p -i.bak -e 'if (m/^projects\[panopoly\]\[version\]/ and not m/7.x/) { s/ = "/ = "7.x-/; }' ssu-lock.make
 git commit -m 'version fix' ssu-lock.make
 
-drush make ssu-lock.make ssu-test
+
+OUTPUTDIR="$CORE-$COREVER-$DATE"
+
+drush make ssu-lock.make $OUTPUTDIR
+
+chgrp -R drupal $OUTPUTDIR
+chmod -R g+w $OUTPUTDIR
+find $OUTPUTDIR -type d -print0 | xargs --null chmod g+s
