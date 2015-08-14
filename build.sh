ba3756b1515048dbe17fc082a496086c027d145b
Eric Eisenhart <eric.eisenhart@sonoma.edu>
2015-08-14 16:36:34 -0700

    Clean up old stuff better

--- build.sh
+++ build.sh
@@ -1,23 +1,31 @@
 #!/bin/sh
 
 DATE=$(date +%Y%m%d)
+LOCK="ssu-lock-$DATE.make"
+
+rm -f $LOCK
 
 git pull
-drush make --no-build ssu.make --lock=ssu-lock.make
+drush make --no-build ssu.make --lock=$LOCK
 
-git commit -m auto ssu-lock.make
-perl -p -i.bak -e 'if (m/^projects\[panopoly\]\[version\]/ and not m/7.x/) { s/ = "/ = "7.x-/; }' ssu-lock.make
-git commit -m 'version fix' ssu-lock.make
+git commit -m auto $LOCK
+perl -p -i -e 'if (m/^projects\[panopoly\]\[version\]/ and not m/7.x/) { s/ = "/ = "7.x-/; }' $LOCK
+git commit -m 'version fix' $LOCK
 
-CORE=$(grep '= "core"' ssu-lock.make | cut -d[ -f2 | cut -d] -f1)
-COREVER=$(grep "projects\[$CORE\]\[version\]" ssu-lock.make | cut -d= -f2 | cut -d'"' -f2)
+CORE=$(grep '= "core"' $LOCK | cut -d[ -f2 | cut -d] -f1)
+COREVER=$(grep "projects\[$CORE\]\[version\]" $LOCK | cut -d= -f2 | cut -d'"' -f2)
 
 OUTPUTDIR="$CORE-$COREVER-$DATE"
 
 rm -rf $OUTPUTDIR
 
-drush make ssu-lock.make $OUTPUTDIR
+drush make $LOCK $OUTPUTDIR
 
 chgrp -R drupal $OUTPUTDIR
 chmod -R g+w $OUTPUTDIR
 find $OUTPUTDIR -type d -print0 | xargs --null chmod g+s
+
+
+echo; echo ; echo
+echo === Your new distribution is: $OUTPUTDIR
+echo === For aegir, use $LOCK
