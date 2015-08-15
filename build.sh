ca67a4400e9fc2439d601889cb4d5eaa9c79afd4
Eric Eisenhart <eric.eisenhart@sonoma.edu>
2015-08-14 17:01:23 -0700

    Getting everything exactly right.

--- build.sh
+++ build.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
 DATE=$(date +%Y%m%d)
-LOCK=ssu-lock.make
+LOCK="ssu-lock.make"
 DATELOCK="ssu-lock-$DATE.make"
 
+# Make sure we have the latest
+git pull
+
+# About to rewrite this
 rm -f $LOCK
 
-git pull
+# Make new locked makefile
 drush make --no-build ssu.make --lock=$LOCK
-git add $LOCK
 
+# Temporary fix for version problem with alternate cores
 perl -p -i -e 'if (m/^projects\[panopoly\]\[version\]/ and not m/7.x/) { s/ = "/ = "7.x-/; }' $LOCK
 
+# Keep this exact version forever
 cp -f $LOCK $DATELOCK
 git add $DATELOCK
 git commit -m auto $DATELOCK
@@ -21,15 +26,16 @@ COREVER=$(grep "projects\[$CORE\]\[version\]" $LOCK | cut -d= -f2 | cut -d'"' -f
 
 OUTPUTDIR="$CORE-$COREVER-$DATE"
 
+# Actually make the output dir
 rm -rf $OUTPUTDIR
-
 drush make $LOCK $OUTPUTDIR
 
 chgrp -R drupal $OUTPUTDIR
 chmod -R g+w $OUTPUTDIR
 find $OUTPUTDIR -type d -print0 | xargs --null chmod g+s
 
-git commit -v $LOCK
+git add $LOCK
+git commit -v ssu.make $LOCK
 
 echo; echo ; echo
 echo === Your new distribution is: $OUTPUTDIR
